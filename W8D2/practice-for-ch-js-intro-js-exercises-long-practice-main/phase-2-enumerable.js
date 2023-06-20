73cea259e106324458e0c4cc67a8364f87c71472
Juan A <avilajajuan@gmail.com>
2023-06-20 17:05:02 -0400

    phase 2

--- W8D2/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
+++ W8D2/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
@@ -1,1 +1,59 @@
 // Your code here
+
+
+Array.prototype.myEach = function(callback) {
+    
+    let result = []
+    for(let i=0; i<this.length; i++){
+       
+        console.log(callback(this[i]));
+    }
+    
+    // return result;
+}
+
+
+// const callback = function(el) {
+//     return el + 1;
+// }
+
+
+
+Array.prototype.myMap = function(callback) {
+    
+    
+    for(let i=0; i<this.length; i++){
+       this[i] = callback(this[i])
+        // console.log(callback(this[i]));
+    }
+    
+    return this;
+}
+
+
+Array.prototype.myReduce = function(callback, ini = null) {
+    
+    if (ini === null){
+        ini = this[0];
+        i=0; 
+    } else{
+        i=1;
+    }
+
+    while(i<this.length){
+       ini = callback(this[i], ini);
+       i++;
+        // console.log(callback(this[i]));
+    }
+    
+    return ini;
+}
+
+
+function callback(el, result){
+    return result * el;
+}
+
+// [1, 2, 3].myReduce(function(acc, el) {
+//     return acc + el;
+//   }, 25); // => 31
